8eef31dac728c508424e00039b76653c0d537f04
ibrahimmasmanaci <ibrahimmasmanaci@gmail.com>
2026-03-08 15:24:10 +0300

    PostMapping example with RequestBody annotation has been added.

--- src/main/java/com/masmanaci/SpringBootFirst/HelloController.java
+++ src/main/java/com/masmanaci/SpringBootFirst/HelloController.java
@@ -1,6 +1,8 @@
 package com.masmanaci.SpringBootFirst;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +13,8 @@ public class HelloController {
         return "Hello World!";
     }
 
+    @PostMapping("/hello")
+    public String hello(@RequestBody String name){
+        return "Hello " + name + " !";
+    }
 }
